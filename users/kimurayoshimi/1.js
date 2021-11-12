2dd05cef18620532b7d9c06932fc679c8549ba92
“Kimura <443n20070511@gmail.com>
2021-11-12 21:41:29 +0900

    [fix] Fixed to be output to console.log

--- users/kimurayoshimi/1.js
+++ users/kimurayoshimi/1.js
@@ -1,15 +1,3 @@
-process.stdin.resume();
-process.stdin.setEncoding("utf8");
-
-let lines = [];
-const reader = require("readline").createInterface({
-  input: process.stdin,
-  output: process.stdout,
-});
-
-reader.on("line", (line) => {
-  lines.push(line);
-});
-reader.on("close", () => {
-  console.log([...lines[0]]);
-});
+const a = (b) => [...b];
+const b = a("ABC");
+console.log(b);
