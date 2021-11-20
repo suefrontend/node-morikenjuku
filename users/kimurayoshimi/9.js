053a37ad023b4bf3a8abae6c6dd5ce7da1fbc509
“Kimura <443n20070511@gmail.com>
2021-11-20 21:51:10 +0900

    [fix] Fix execution functions.

--- users/kimurayoshimi/9.js
+++ users/kimurayoshimi/9.js
@@ -2,7 +2,6 @@ const a = (b) => b.includes("e");
 const b = a(["a", "b", "c"]);
 console.log(b);
 
-const c = (d) => d.find((value) => value === "e");
-const d = a(["a", "b", "c"]);
+const c = (d) => d.every((value) => value === "e");
+const d = c(["a", "b", "c"]);
 console.log(d);
-
