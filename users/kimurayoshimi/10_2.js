7174e1540308131dc356d63594cabab8b8c09e08
“Kimura <443n20070511@gmail.com>
2021-11-23 10:29:47 +0900

    [feat]task10 Code for reduce only

---
+++ users/kimurayoshimi/10_2.js
@@ -0,0 +1,16 @@
+const a = (b) =>
+  b.reduce((target, key) => {
+    target["ids"] = b.map((value) => value.id);
+    target["entities"] = {
+      ...target["entities"],
+      [key.id]: { value: key.value },
+    };
+    return target;
+  }, {});
+
+const b = a([
+  { id: "a", value: 1 },
+  { id: "b", value: 2 },
+  { id: "c", value: 3 },
+]);
+console.log(b);
