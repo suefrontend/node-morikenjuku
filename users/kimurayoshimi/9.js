0c00629d870bcf0473e4ab58fd9f61d57acc736a
KimuraYoshimi <43788628+443n0511@users.noreply.github.com>
2021-11-30 19:26:03 +0900

    [fix] Fixing method writing errors

--- users/kimurayoshimi/9.js
+++ users/kimurayoshimi/9.js
@@ -1,7 +1,11 @@
 const a = (b) => b.includes("e");
-const b = a(["a", "b", "c"]);
+const b = a(["a", "b", "c", "e"]);
 console.log(b);
 
-const c = (d) => d.every((value) => value === "e");
-const d = c(["a", "b", "c"]);
+const c = (d) => d.some((value) => value === "e");
+const d = c(["a", "b", "c", "e"]);
 console.log(d);
+
+const e = (f) => f.find((value) => value === "e");
+const f = e(["a", "b", "c", "e"]);
+console.log(f);
