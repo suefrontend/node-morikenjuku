d067e011484f85ec9412bafaad0c062b508abc76
KimuraYoshimi <43788628+443n0511@users.noreply.github.com>
2021-11-30 19:41:05 +0900

    [fix] Delete the "e".

--- users/kimurayoshimi/9.js
+++ users/kimurayoshimi/9.js
@@ -1,11 +1,11 @@
 const a = (b) => b.includes("e");
-const b = a(["a", "b", "c", "e"]);
+const b = a(["a", "b", "c"]);
 console.log(b);
 
 const c = (d) => d.some((value) => value === "e");
-const d = c(["a", "b", "c", "e"]);
+const d = c(["a", "b", "c"]);
 console.log(d);
 
 const e = (f) => f.find((value) => value === "e");
-const f = e(["a", "b", "c", "e"]);
+const f = e(["a", "b", "c"]);
 console.log(f);
