c82cda76f48908e9c2a25b33ea916243221b582e
KimuraYoshimi <43788628+443n0511@users.noreply.github.com>
2021-12-01 22:07:55 +0900

    [fix]  Delete find

--- users/kimurayoshimi/9.js
+++ users/kimurayoshimi/9.js
@@ -6,6 +6,3 @@ const c = (d) => d.some((value) => value === "e");
 const d = c(["a", "b", "c"]);
 console.log(d);
 
-const e = (f) => f.find((value) => value === "e");
-const f = e(["a", "b", "c"]);
-console.log(f);
