91d9866a4ad676cc2e01b15bb1815962245dfd6f
Kaya0312 <oyama_kayano@yahoo.co.jp>
2021-12-11 03:23:56 -0800

    feat: add another solution

--- users/kayanooyama/1.js
+++ users/kayanooyama/1.js
@@ -1,9 +1,19 @@
-function input(param) {
+function input1(param) {
 	const str = param.split('');
 
 	return str;
 }
-console.log(input('abc'));
+console.log(input1('abc'));
+
+function input2(param) {
+	const arr = [];
+	for (var i = 0; i < param.length; i++) {
+		arr.push(param[i]);
+	}
+
+	return arr;
+}
+console.log(input2('abc'));
 
 // input: "abc"
 // output: ["a", "b", "c"]
