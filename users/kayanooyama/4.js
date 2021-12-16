ad0e03f9117a26a12812349fae652904558119da
Kaya0312 <oyama_kayano@yahoo.co.jp>
2021-12-16 14:54:46 -0800

    feat: create a function which returns odd numbers in array

--- users/kayanooyama/4.js
+++ users/kayanooyama/4.js
@@ -1,8 +1,16 @@
 function input(param) {
 	// Return odd numbers
-	return;
+	const arr = [];
+
+	param.forEach((el) => {
+		if (el % 2 !== 0) {
+			arr.push(el);
+		}
+	});
+
+	return arr;
 }
-console.log(input('[1, 2, 3, 4, 5, 6, 7]'));
+console.log(input([1, 2, 3, 4, 5, 6, 7]));
 
 // input: [1, 2, 3, 4, 5, 6, 7]
 // output: [1, 3, 5, 7]
